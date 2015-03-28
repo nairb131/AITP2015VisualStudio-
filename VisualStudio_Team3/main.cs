32cee6b61e861b942868f2caed749dfaf36bd14f
nairb131 <nairb131@gmail.com>
2015-03-27 19:25:17 -0800

    asda
    dasda

--- VisualStudio_Team3/main.cs
+++ VisualStudio_Team3/main.cs
@@ -25,26 +25,26 @@ namespace VisualStudio_Team3
 
             string game = cb_game.SelectedItem.ToString();
 
-            if (section < 107 || section >= 118 && section <= 124)
-            {
-                ticketprice = 38;
-            }
-            if (section < 200)
-            {
-                ticketprice = 37;
-            }
-            else
-            {
-                ticketprice = 36;
-            }
-            if (game == "Game 5" || game == "Game 7" || game == "Game 11")
-            {
-                ticketprice -= 5;
-            }
-            if (game == "Game 6" || game == "Game 8" || game == "Game 9" || game == "Game 10" || game == "Game 12" )
-            {
-                ticketprice -= 3;
-            }
+            //if (section < 107 || section >= 118 && section <= 124)
+            //{
+            //    ticketprice = 38;
+            //}
+            //if (section < 200)
+            //{
+            //    ticketprice = 37;
+            //}
+            //else
+            //{
+            //    ticketprice = 36;
+            //}
+            //if (game == "Game 5" || game == "Game 7" || game == "Game 11")
+            //{
+            //    ticketprice -= 5;
+            //}
+            //if (game == "Game 6" || game == "Game 8" || game == "Game 9" || game == "Game 10" || game == "Game 12" )
+            //{
+            //    ticketprice -= 3;
+            //}
         }
 
         private void main_Load(object sender, EventArgs e)
