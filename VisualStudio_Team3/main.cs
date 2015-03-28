65ab409ff1ab17575d635f8deaec72fdb5cd2970
nairb131 <nairb131@gmail.com>
2015-03-27 19:23:24 -0800

    qwedqw
    sdfsdf

--- VisualStudio_Team3/main.cs
+++ VisualStudio_Team3/main.cs
@@ -21,15 +21,15 @@ namespace VisualStudio_Team3
         {
             decimal ticketprice;
 
-            int section = int(cb_section.SelectedItem);
+            string section = cb_section.SelectedItem.ToString();
 
-            string game = string(cb_game.SelectedItem);
+            string game = cb_game.SelectedItem.ToString();
 
-            if (section < 107)
+            if (section < 107 || section >= 118 && section <= 124)
             {
                 ticketprice = 38;
             }
-            if (section >= 107 && section <= 117)
+            if (section < 200)
             {
                 ticketprice = 37;
             }
@@ -37,17 +37,13 @@ namespace VisualStudio_Team3
             {
                 ticketprice = 36;
             }
-            if (game == "Game 1" || game == "Game 2" || game == "Game 3" || game == "Game 4" || game == "Finals 1" || game == "Finals 2" )
-            {
-
-            }
             if (game == "Game 5" || game == "Game 7" || game == "Game 11")
             {
-
+                ticketprice -= 5;
             }
             if (game == "Game 6" || game == "Game 8" || game == "Game 9" || game == "Game 10" || game == "Game 12" )
             {
-
+                ticketprice -= 3;
             }
         }
 
